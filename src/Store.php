4ad03fb452b7a8c4c1e0f3217190225f31786801
aimenkhakwani <aimen.khakwani@hotmail.com>
2016-09-30 08:50:18 -0700

    Add passing tests for save, getAll and deleteAll functions

--- src/Store.php
+++ src/Store.php
@@ -37,6 +37,29 @@
             return $this->id;
         }
 
-        
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO stores (name, address) VALUES ('{$this->getName()}', '{$this->getAddress()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_stores = $GLOBALS['DB']->query("SELECT * FROM stores;");
+            $stores = array();
+            foreach($returned_stores as $store) {
+                $name = $store['name'];
+                $address = $store['address'];
+                $id = $store['id'];
+                $new_store = new Store($name, $address, $id);
+                array_push($stores, $new_store);
+            }
+            return $stores;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stores;");
+        }
     }
 ?>
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -13,6 +13,11 @@
 
     class StoreTest extends PHPUnit_Framework_TestCase
     {
+        protected function tearDown()
+        {
+            Store::deleteAll();
+        }
+
         function test_getName()
         {
             //Arrange
@@ -25,6 +30,91 @@
 
             //Assert
             $this->assertEquals($name, $result);
+        }
+
+        function test_getAddress()
+        {
+            //Arrange
+            $name = "Shoes Galore";
+            $address = "9718 NE 8th Street";
+            $store = new Store($name, $address);
+
+            //Act
+            $result = $store->getAddress();
+
+            //Assert
+            $this->assertEquals($address, $result);
+        }
+
+        function test_getId()
+        {
+            //Arrange
+            $name = "Shoes Galore";
+            $address = "9718 NE 8th Street";
+            $id = 14;
+            $store = new Store($name, $address, $id);
+
+            //Act
+            $result = $store->getId();
+
+            //Assert
+            $this->assertEquals($id, $result);
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $name = "Shoes Galore";
+            $address = "9718 NE 8th Street";
+            $store = new Store($name, $address);
+            $store->save();
+
+            //Act
+            $result = Store::getAll();
+
+            //Assert
+            $this->assertEquals($store, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $name = "Shoes Galore";
+            $address = "9718 NE 8th Street";
+            $store = new Store($name, $address);
+            $store->save();
+
+            $name2 = "Shoes Galore";
+            $address2 = "9718 NE 8th Street";
+            $store2 = new Store($name2, $address2);
+            $store2->save();
+
+            //Act
+            $result = Store::getAll();
+
+            //Assert
+            $this->assertEquals([$store, $store2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $name = "Shoes Galore";
+            $address = "9718 NE 8th Street";
+            $store = new Store($name, $address);
+            $store->save();
+
+            $name2 = "Shoes Galore";
+            $address2 = "9718 NE 8th Street";
+            $store2 = new Store($name2, $address2);
+            $store2->save();
+
+            //Act
+            Store::deleteAll();
+            $result = Store::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
         }
     }
 ?>
