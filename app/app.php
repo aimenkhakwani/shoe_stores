4bf9ede76ed7893dc996cbda53e14904804d771a
aimenkhakwani <aimen.khakwani@hotmail.com>
2016-09-30 09:59:16 -0700

    Add framework and homa page route in app.php

---
+++ app/app.php
@@ -0,0 +1,29 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Store.php";
+    require_once __DIR__."/../src/Brand.php";
+
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    $server = 'mysql:host=localhost;dbname=stores';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+      return $app['twig']->render("index.html.twig");
+    });
+
+    return $app;
+?>
